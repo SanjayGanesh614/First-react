e8d67b6adba05f04556cc827c03b843b8e6d87f3
Sanjay Ganesh K Barade <sgbarade@gmail.com>
2025-07-31 01:17:19 +0530

    Updated app.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 export default function App() {
+    const name = "Sanjay";
+    const age = 19;
     return(
         <div>
-            <h1>Hello I am Sanjay</h1>
+            <h1>Hello I am {name} and I am {age} </h1>
             <p>Welcome to my first React App</p>
             <MyButton />
             <p>Enjoy your stay!</p>
+            <Welcome name="USER" />
+            
         </div>
     );
 }
@@ -15,3 +19,8 @@ function MyButton() {
         </button>
     );
 }
+function Welcome(props) {
+    return <h2>Welcome, {props.name}</h2>;
+}
+
+
